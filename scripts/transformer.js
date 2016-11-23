42fe5987bceaccf7189199694ad93a26fdc6f312
James Ide <ide@jameside.com>
2016-11-23 02:00:44 -0800

    Make the transformer regex work for Windows path separators too

--- scripts/transformer.js
+++ scripts/transformer.js
@@ -3,8 +3,9 @@ const transformer = require('react-native/packager/transformer');
 module.exports = (data, callback) => {
   // Skip transpiling libraries that run without transpilation:
   //  * AmmoNext: a massive JS file produced by emscripten
-  //  * WHS: already transpiled
-  if (/\/node_modules\/(ammonext|lodash|three|three-orbit-controls)\//.test(data.filename)) {
+  //  * three.js: Already compiled
+  //  * three-orbit-controls: unused
+  if (/[\/\\]node_modules[\/\\](ammonext|lodash|three|three-orbit-controls)[\/\\]/.test(data.filename)) {
     callback(null, { code: data.sourceCode });
   } else {
     transformer(data, callback);
